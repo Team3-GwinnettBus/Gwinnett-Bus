27dbd90699e8e8b8d04f3998ad88988e335cef1e
michaelrzg <michaelrizig@gmail.com>
2024-09-17 21:27:22 -0400

    added dataflow diagram

--- Back-End/producer.py
+++ Back-End/producer.py
@@ -1,9 +1,20 @@
 from kafka import KafkaProducer
-
+import json
+import time
 TOPICNAME = 'GCPS_Bus_Monitoring'
 SERVERIP = 'localhost:9092'
-
-producer = KafkaProducer(bootstrap_servers=SERVERIP)
-producer.send(TOPICNAME,b'test')
-producer.flush()
-print("sent")
+lat = 37.8191
+producer = KafkaProducer(bootstrap_servers=SERVERIP,value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+for i in range(10):
+    lat = lat + .1
+    time.sleep(1.5)
+    producer.send(TOPICNAME,{
+        "id" : 1,
+        "latitude" : lat,
+        "longitude" : -84.0155,
+        "heading" : 90,
+        "accuracy" : 20,
+        "speed" : 55
+        } )
+    producer.flush()
+print("sent")   

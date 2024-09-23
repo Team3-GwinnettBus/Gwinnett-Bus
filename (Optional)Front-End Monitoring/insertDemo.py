ec0ce857869b8056eea872f17f6537a15ca432f0
Michael Rizig <michael@RHEL9.xVM>
2024-09-23 15:31:43 -0400

    added back insert demo and other

--- (Optional)Front-End Monitoring/insertDemo.py
+++ (Optional)Front-End Monitoring/insertDemo.py
@@ -8,12 +8,13 @@ ENDPOINT = 'http://localhost:3000/setBusData'
 
 #format incoming data as json based on formatting instructions (see README)
 busdata = {
-    "id" : 1,
+    "BusID" : 1,
     "latitude" : 33.8191,
     "longitude" : -84.0155,
     "heading" : 90,
     "accuracy" : 20,
-    "speed" : 55
+    "speed" : 55,
+    
     } 
 
 post = requests.post(url=ENDPOINT,json = busdata)

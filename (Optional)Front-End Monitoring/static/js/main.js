6f01d52f7e025099142dbb2a4d750eb5e295178e
michaelrzg <michaelrizig@gmail.com>
2024-10-26 19:29:59 -0400

    added recenter button

--- (Optional)Front-End Monitoring/static/js/main.js
+++ (Optional)Front-End Monitoring/static/js/main.js
@@ -113,7 +113,7 @@ function endLoader() {
   // hide the loader and show the updated info panel
   document.getElementById("loader").style.display = "none";
   document.getElementById("info").style.display = "block";
-  document.getElementById("recenter").onclick((e) => {
+  document.getElementById("recenter").addEventListener("click", () => {
     map.setView([33.953470353472376, -84.02841567993165]);
   });
 }

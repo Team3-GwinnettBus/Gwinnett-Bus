bc15f76f91bd84a2b1ca5ecfe578e0bfa47846f1
Michael Rizig <136545654+michaelrzg@users.noreply.github.com>
2024-11-10 19:30:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@
 cd ~
 cd Gwinnett-Bus
 cd 'kafka_2.13-3.8.0'
-bin/kafka-server-start.sh config/kraft/server.properties
-sleep 20
-
-#start server
-cd ~
-cd Gwinnett-Bus/
-cd '(Optional)Front-End Monitoring'
-python main.py            
+bin/kafka-server-start.sh config/kraft/server.properties &
+sleep 10
 
 #start bus locations simulations
 cd ~
 cd Gwinnett-Bus/Back-End/Producer/
 ./bussim.sh &
+
+#start server
+cd ~
+cd Gwinnett-Bus/
+cd '(Optional)Front-End Monitoring'
+python main.py     
